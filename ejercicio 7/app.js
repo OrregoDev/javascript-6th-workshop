67c9239182bba44f0eadc98a70178796685bd0a5
OrregoDev <camilo.orrego554@gmail.com>
2024-04-29 20:29:39 -0500

    WIP ejercicio 7

---
+++ ejercicio 7/app.js
@@ -0,0 +1,40 @@
+// Ejercicio 7
+
+const main = () => {
+  alert(
+    'Teniendo en cuenta el codigo de la imagen, predice el orden en que se mostraran los mensajes en consola separados por comas:'
+  );
+
+  const userAnswer = prompt('');
+
+  const realResult = [
+    'Inicio del script',
+    'Fin del script',
+    'Promesa resuelta',
+    'Primer setTimeout',
+    'Segundo setTimeout',
+  ];
+
+  const mistakes = [];
+
+  userAnswer.split(',').forEach((prediction, index) => {
+    if (prediction.trim() !== realResult[index]) {
+      mistakes.push(
+        `El paso ${
+          index + 1
+        } es incorrecto. Predicción: ${prediction}, Resultado real: ${
+          realResult[index]
+        }`
+      );
+    }
+  });
+
+  if (mistakes.length === 0) {
+    alert('¡Felicitaciones! Has acertado el orden.');
+  } else if (mistakes.length === 1) {
+    console.log('Has fallado en un solo paso:', mistakes[0]);
+  } else {
+    console.log('Has fallado en varios pasos:', mistakes.join(', '));
+  }
+};
+setTimeout(main, 200);
