941dbb5eeb5dff301e2216c9d9d06f0ab2b21ead
OrregoDev <camilo.orrego554@gmail.com>
2024-04-30 18:51:26 -0500

    finish ejercicio 8

--- ejercicio 8/app.js
+++ ejercicio 8/app.js
@@ -1,1 +1,18 @@
 // Ejercicio 8
+
+function createCounter(currentValue) {
+  return function () {
+    return currentValue++;
+  };
+}
+
+let increaseCounter = createCounter(1);
+while (true) {
+  let option = confirm('¿Deseas incrementar el contador?');
+  if (option) {
+    let newValue = increaseCounter();
+    alert('El valor actual del contador es: ' + newValue);
+  } else {
+    break;
+  }
+}
