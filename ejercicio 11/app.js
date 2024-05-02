19f96bdd0b2141fd5ef4362a7c98931e37895884
OrregoDev <camilo.orrego554@gmail.com>
2024-05-02 15:26:33 -0500

    wip ejercicio 11

--- ejercicio 11/app.js
+++ ejercicio 11/app.js
@@ -1,50 +1,47 @@
 // Ejercicio 11
 
-// Ruta del archivo data.json
-const url = 'data.json'; // Cambiar por la ruta correcta
+const url = 'data.json';
 
-// Función para cargar y mostrar el contenido de data.json
-function cargarYMostrarData() {
-  // Retorna una nueva promesa que se resuelve después del setTimeout
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      // Realiza la solicitud fetch dentro del setTimeout
-      fetch(url)
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error('Error al cargar los datos.');
-          }
-          return response.json();
-        })
-        .then((data) => {
-          console.log('Habitaciones:', data.rooms);
-          console.log('Tipos de Habitaciones:', data.roomTypes);
-          resolve(data); // Resuelve la promesa con los datos cargados
-        })
-        .catch((error) => {
-          console.error(error);
-          reject(error); // Rechaza la promesa si hay un error
-        });
-    }, 3000);
-  });
+function getNumber(promptMessage, options = {}) {
+  while (true) {
+    const userInput = prompt(promptMessage);
+    if (options?.allowedNullish && !userInput) return userInput;
+    const castedInput = parseInt(userInput);
+    if (!Number.isNaN(castedInput)) return castedInput;
+    alert('Please enter a valid number.');
+  }
 }
 
-// Llamar a la función para cargar y mostrar el contenido de data.json
-cargarYMostrarData()
-  .then(({ rooms, roomTypes }) => {
-    // Mostrar el contenido de las habitaciones después de cargar los datos
-    const userInput = prompt(
-      'Ingrese el numero de habitacion a reservar: ' +
-        rooms
-          .map((room) => {
-            return `\nRoom Number: ${room.number} (${
-              roomTypes.find((type) => type.id === room.roomTypeId).name
-            })`;
-          })
-          .join(', ')
-    );
-    // ... Continuar con la lógica de la app
-  })
-  .catch((error) => {
-    console.error('Error al manejar la promesa:', error);
-  });
+function getOption({
+  optionsArray,
+  promptMessage,
+  invalidMessage = 'Please enter a valid option.',
+}) {
+  const formattedListString = optionsArray
+    .map((element, index) => `${index + 1}. ${element}`)
+    .join('\n');
+
+  while (true) {
+    const option = getNumber(`${promptMessage}\n` + formattedListString);
+    const isValidOption = option && option > 0 && option <= optionsArray.length;
+    if (isValidOption) return optionsArray[option - 1];
+    alert(invalidMessage);
+  }
+}
+
+function getDate({ promptMessage, timeString }) {
+  const dateFormat = 'yyyy-mm-dd';
+  while (true) {
+    const date = prompt(`${promptMessage} Format: ${dateFormat}`) + timeString;
+    const isValidDate = !Number.isNaN(Date.parse(dateInput));
+    if (isValidDate) return new Date(dateInput);
+    alert('Please enter a valid date.');
+  }
+}
+
+function idGenerator(initialId) {
+  return function () {
+    return initialId++;
+  };
+}
+const generateId = idGenerator(1);
