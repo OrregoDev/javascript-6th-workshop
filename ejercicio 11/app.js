fa2ee324e37fc52c6233d45c065d2092cc2e15f9
OrregoDev <camilo.orrego554@gmail.com>
2024-05-03 21:45:05 -0500

    Finish ejercicio 11

--- ejercicio 11/app.js
+++ ejercicio 11/app.js
@@ -15,7 +15,7 @@ function getNumber(promptMessage, options = {}) {
 }
 
 function getDate({ promptMessage, timeString = 'T13:00:00' }) {
-  const dateFormat = 'yyyy-mm-dd';
+  const dateFormat = 'YYYY-MM-DD';
   while (true) {
     const dateInput =
       prompt(`${promptMessage} Format: ${dateFormat}`) + timeString;
@@ -36,7 +36,7 @@ function showMenu() {
   let isMenuOpened = true;
   while (isMenuOpened) {
     const userOption = getNumber(
-      'Welcome to our booking system! \nPlease enter the number of the action that you would like to do: \n\n1.Book \n2.Check available rooms \n3.Check my bookings \n4.Cancel booking \n5.Modify booking \n6.Exit'
+      'Welcome to our booking system! \nPlease enter the number of the action that you would like to do: \n\n1. Create a booking \n2. Check room availability \n3. Check my bookings \n4. Cancel booking \n5. Modify booking \n6. Exit'
     );
     switch (userOption) {
       case 6: {
@@ -80,10 +80,10 @@ function showMenu() {
         // 6. Ask for the dates
         let checkInDate, checkOutDate;
         while (true) {
-          const checkInDate = getDate({
+          checkInDate = getDate({
             promptMessage: 'Please enter the check in date of your booking: ',
           });
-          const checkOutDate = getDate({
+          checkOutDate = getDate({
             promptMessage: 'Please enter the check out date of your booking: ',
           });
           if (checkOutDate > checkInDate) {
@@ -93,14 +93,155 @@ function showMenu() {
         }
         const booking = {
           id: generateId(),
+          roomNumber,
           guestName,
+          checkInDate,
+          checkOutDate,
+          guestsNumber,
         };
-        // 6. Create booking and change room availability
+        bookings.push(booking);
+        roomToBook.availability = false;
+
         // 7. Let the user know that thet booking was succesfully made
+        alert(
+          `The room ${roomNumber} has been booked succesfully on behalf of ${guestName}`
+        );
+        break;
+      }
+
+      case 2: {
+        let roomToCheck;
+        // Confirm if the room has the capacity for the number of guests
+        const guestsNumber = getNumber(
+          'Please enter the number of guest that are going to stay in the room'
+        );
+        if (roomTypes.capacity < guestsNumber) {
+          alert('The room does not have the capacity');
+        }
+        const roomNumber = getNumber(
+          'Enter the number of the room to check its availability'
+        );
+        while (true) {
+          roomToCheck = rooms.find((room) => room.number === roomNumber);
+          if (roomToCheck) break;
+          alert('Room number invalid, please try again');
+        }
+        // Check if the room is available
+        const bookingExists = bookings.find(
+          (booking) => booking.roomNumber === roomNumber
+        );
+        if (bookingExists) {
+          alert('The room is already booked');
+        } else {
+          alert('The room is available.');
+        }
+      }
+      case 3: {
+        const guestName = prompt('Enter your full name please:').toLowerCase();
+        const guestBookings = bookings.filter(
+          (booking) => booking.guestName === guestName
+        );
+        guestBookings.forEach((booking) => {
+          console.log(
+            `Booking ID: ${booking.id} \nRoom number: ${
+              booking.roomNumber
+            } \nGuest name: ${
+              booking.guestName
+            } \nCheck in date: ${booking.checkInDate.toLocaleDateString()} \nCheck out date: ${booking.checkOutDate.toLocaleDateString()} \nGuests number: ${
+              booking.guestsNumber
+            }`
+          );
+        });
+        break;
+      }
+      case 4: {
+        const guestName = prompt('Enter your full name please:').toLowerCase();
+        const guestBookings = bookings.filter(
+          (booking) => booking.guestName === guestName
+        );
+        const formattedGuestBookings = guestBookings.map((booking) => {
+          return `Booking ID: ${booking.id} \nRoom number: ${
+            booking.roomNumber
+          } \nGuest name: ${
+            booking.guestName
+          } \nCheck in date: ${booking.checkInDate.toLocaleDateString()} \nCheck out date: ${booking.checkOutDate.toLocaleDateString()} \nGuests number: ${
+            booking.guestsNumber
+          }`;
+        });
+
+        let bookingId, bookingToCancel;
+        while (true) {
+          bookingId = getNumber(
+            'Enter the ID of the booking to cancel:\n' +
+              formattedGuestBookings.join('\n\n')
+          );
+          bookingToCancel = guestBookings.find(
+            (booking) => booking.id === bookingId
+          );
+          if (bookingToCancel) break;
+          alert('There are no bookings that match this ID, please try again');
+        }
+        const bookingIndex = guestBookings.findIndex(
+          (booking) => booking.id === bookingToCancel.id
+        );
+        bookings.splice(bookingIndex, 1);
+        alert(
+          `The booking with ID ${bookingToCancel.id} was succesfully canceled.`
+        );
+      }
+      case 5: {
+        const guestName = prompt('Enter your full name please:').toLowerCase();
+        const guestBookings = bookings.filter(
+          (booking) => booking.guestName === guestName
+        );
+        const formattedGuestBookings = guestBookings.map((booking) => {
+          return `Booking ID: ${booking.id} \nRoom number: ${
+            booking.roomNumber
+          } \nGuest name: ${
+            booking.guestName
+          } \nCheck in date: ${booking.checkInDate.toLocaleDateString()} \nCheck out date: ${booking.checkOutDate.toLocaleDateString()} \nGuests number: ${
+            booking.guestsNumber
+          }`;
+        });
+
+        let bookingId, bookingToEdit;
+        while (true) {
+          bookingId = getNumber(
+            'Enter the ID of the booking to edit:\n' +
+              formattedGuestBookings.join('\n\n')
+          );
+          bookingToEdit = guestBookings.find(
+            (booking) => booking.id === bookingId
+          );
+          if (bookingToEdit) break;
+          alert('There are no bookings that match this ID, please try again');
+        }
+        const bookingIndex = guestBookings.findIndex(
+          (booking) => booking.id === bookingToEdit.id
+        );
+        let newCheckInDate, newCheckOutDate;
+        while (true) {
+          newCheckInDate = getDate({
+            promptMessage: 'Please enter the check in date of your booking: ',
+          });
+          newCheckOutDate = getDate({
+            promptMessage: 'Please enter the check out date of your booking: ',
+          });
+          if (newCheckOutDate > newCheckInDate) {
+            break;
+          }
+          alert('The check out date must be before the check in date.');
+        }
+        bookingToEdit.checkInDate = newCheckInDate;
+        bookingToEdit.checkOutDate = newCheckOutDate;
+        alert(
+          `The booking with ID ${bookingToEdit.id} was succesfully modified.`
+        );
       }
     }
   }
 }
+
 function loadAndShowData() {
   // Retorna una nueva promesa que se resuelve después del setTimeout
   return new Promise((resolve, reject) => {
